f475bfdcdf79a4ef1c809d6fb7e076137d36d12d
Nuno Miguel Mvemba <nunosirijc23@gmail.com>
2023-02-28 14:19:32 +0100

    <General> entity Ticket modified

--- src/modules/events/entity/ticket.js
+++ src/modules/events/entity/ticket.js
@@ -1,21 +1,25 @@
 const { v4: uuidV4 } = require('uuid');
 
 class Ticket {
-    constructor(payment, userId, eventId) {
+    constructor(payment, userId, eventId, bankReceiptDirectory, isApproved) {
         if (!this.id) {
             this.id = uuidV4();
         }
         this.payment = payment;
         this.userId = userId;
         this.eventId = eventId;
+        this.bankReceiptDirectory = bankReceiptDirectory;
+        this.isApproved = isApproved;
         this.createdAt = new Date();
     }
 
-    setValues(id, payment, userId, eventId, createdAt) {
+    setValues(id, payment, userId, eventId, bankReceiptDirectory, isApproved, createdAt) {
         this.id = id;
         this.payment = payment;
         this.userId = userId;
         this.eventId = eventId;
+        this.bankReceiptDirectory = bankReceiptDirectory;
+        this.isApproved = isApproved;
         this.createdAt = createdAt;
     }
 }
