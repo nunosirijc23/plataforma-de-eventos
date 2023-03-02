33d8941544f75100c6073da08ef6d5479af889b9
Nuno Miguel Mvemba <nunosirijc23@gmail.com>
2023-03-02 12:14:03 +0100

    <General> ticket interface  modified

--- src/modules/events/repositories/ITicketRepository.js
+++ src/modules/events/repositories/ITicketRepository.js
@@ -2,6 +2,7 @@ class ITicketRepository {
     create(ticket) {}
     findAllByEventId(eventId) {}
     findAllByUserId(userId) {}
+    findAllTicketsBoughtByEventId(eventId) {}
 }
 
 module.exports = ITicketRepository;
